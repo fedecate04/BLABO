25b453cba39615a41745fe31d9f5b588d933089e
fedecate04 <fede.catereniuc@gmail.com>
2025-05-17 22:07:12 -0300

    Update blabotank.py

--- blabotank.py
+++ blabotank.py
@@ -394,9 +394,9 @@ if calcular:
                 st.write(f"• {k}: **{v}**")
 
        pdf_bytes = BytesIO()
-pdf_bytes.write(generar_pdf_pedagogico(resultados, ecuaciones, explicaciones))
-pdf_bytes.seek(0)
-st.download_button("📥 Descargar informe PDF", data=pdf_bytes, file_name="informe_blabo.pdf", mime="application/pdf")
+       pdf_bytes.write(generar_pdf_pedagogico(resultados, ecuaciones, explicaciones))
+       pdf_bytes.seek(0)
+       st.download_button("📥 Descargar informe PDF", data=pdf_bytes, file_name="informe_blabo.pdf", mime="application/pdf")
 
             
 
